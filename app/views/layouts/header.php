c09d24fb9aded7a990de3f2787579f0a2250e31a
Maharramoff <sh.maharramov@gmail.com>
2020-03-05 23:08:57 +0400

    chore: auth links in header

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Bare - Start Bootstrap Template</title>
+    <title>BeeJee Task</title>
 
     <!-- Bootstrap core CSS -->
     <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -20,8 +20,9 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
     <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="#">Task App</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
@@ -32,13 +33,21 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
+                    <?php
+
+                    if ($user)
+                    {
+                        ?>
+                        <a class="nav-link" href="/auth/logout">Logout</a>
+                        <?php
+                    }
+                    else
+                    {
+                        ?>
+                        <a class="nav-link" href="/auth/login">Login</a>
+                        <?php
+                    }
+                    ?>
                 </li>
             </ul>
         </div>
