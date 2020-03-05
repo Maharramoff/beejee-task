b7d2d37bb2b4182a1926fdb28dcb8d8c857387ec
Maharramoff <sh.maharramov@gmail.com>
2020-03-05 20:55:25 +0400

    add: base layouts, bootstrap/jquery

---
+++ app/views/layouts/footer.php
@@ -0,0 +1,7 @@
+<!-- Bootstrap core JavaScript -->
+<script src="/assets/vendor/jquery/jquery.slim.min.js"></script>
+<script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ app/views/task/show.php
@@ -0,0 +1,9 @@
+<!-- Page Content -->
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <p class="lead"><?=$foo?></p>
+        </div>
+    </div>
+</div>
+
