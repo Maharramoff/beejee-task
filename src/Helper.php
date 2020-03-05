a8aed5bca0931454aee99fd7b222f628d838babf
Maharramoff <sh.maharramov@gmail.com>
2020-03-06 00:35:26 +0400

    chore: redirect with delay

--- src/Helper.php
+++ src/Helper.php
@@ -11,12 +11,19 @@ final class Helper
      *
      * @param string $uri URL
      * @param int $code HTTP Response status code
+     * @param int $seconds
      * @return void
      */
-    public static function redirect(string $uri, int $code = 302): void
+    public static function redirect(string $uri, int $seconds = 0, int $code = 302): void
     {
         if (false === headers_sent())
         {
+            if($seconds > 0)
+            {
+                header("refresh:" . $seconds . "; url=" . $uri, true, $code);
+                exit;
+            }
+
             header('Location: ' . $uri, true, $code);
         }
 
