d932b14c84f83e73452e3689e3e3210b143053bb
Maharramoff <sh.maharramov@gmail.com>
2020-03-06 04:03:47 +0400

    update: request parameterbag property in main controller

--- src/Controller.php
+++ src/Controller.php
@@ -9,10 +9,12 @@ abstract class Controller
 {
     protected $view;
     protected $request;
+    protected $parameterBag;
 
     public function __construct()
     {
         $this->view = new View();
         $this->request = Request::createFromGlobals();
+        $this->parameterBag = $this->request->request;
     }
 }
